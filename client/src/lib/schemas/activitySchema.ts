36f821e52895c9348124eb63d1c0ab5a986703b3
David Stepanic <davidstepanic9@gmail.com>
2025-08-07 13:56:27 +0200

    End of section 11

---
+++ client/src/lib/schemas/activitySchema.ts
@@ -0,0 +1,21 @@
+import {z} from 'zod';
+
+const required = (name: string) => 
+    z.string().trim().min(1, { message: `${name} is required`  })
+
+export const activitySchema = z.object({
+    title: required('Title'),
+    description: required('Description'),
+    category: required('Category'),
+    date: z.coerce.date<Date>({
+        message: 'Date is required'
+    }),  
+    location: z.object({
+        venue: required('Venue'),
+        city: z.string().optional(),
+        latitude: z.coerce.number(), 
+        longitude: z.coerce.number()
+    })
+})
+
+export type ActivitySchema = z.input<typeof activitySchema>;
